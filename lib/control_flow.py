238e0d90c483cd25bd9e5ebaddb1cb4a7b40d01f
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-15 20:50:51 +0300

    Control Flow: Conditional Statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == 'admin' or username == 'ADMIN') and password == '12345':
+        return "Access granted"
+    else:
+        return "Access denied"
+print(admin_login("admin", "12345"))
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40 :
+        response="brisk"
+    elif 40<= temperature < 65 :
+        response="a little chilly"
+    elif temperature >= 85:
+        response="too dang hot"
+    else:
+        response="perfect"
+    return f"It's {response} out there!"
+print(hows_the_weather(33))
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        result = "FizzBuzz"
+    elif num % 3 == 0:
+        result = "Fizz"
+    elif num % 5 ==0:
+        result = "Buzz"
+    else:
+        result = num
+    return result
+print(fizzbuzz(1))
+print(fizzbuzz(55))
+print(fizzbuzz(30))
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "/":
+        result = num1 / num2
+    elif operation == "*":
+        result =num1 * num2
+    else:
+        print("Invalid operation!")
+        return None
+    return result
+print(calculator("-",7,1))
